b2019a950823ea051ce55010b76cd7d6f8ab3145
liuyong <liuyong.ly@bytedance.com>
2018-09-02 16:33:09 +0800

    chore:xxx

--- src/fibonacci/Main.java
+++ src/fibonacci/Main.java
@@ -1,4 +1,4 @@
-package first;
+package fibonacci;
 
 import java.util.Scanner;
 
@@ -13,8 +13,8 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.print("请输入月数：");
         int month = scanner.nextInt();
-//        System.out.println(month + "个月后，兔子的总对数为：" + Fibonacci.fibonacciByRecursion(month));
+        System.out.println(month + "个月后，兔子的总对数为：" + Fibonacci.fibonacciByRecursion(month));
 //        System.out.println(month + "个月后，兔子的总对数为：" + Fibonacci.fibonacciByLoop(month));
-        System.out.println(month + "个月后，兔子的总对数为：" + Fibonacci.fibonacciByMatrix(month));
+//        System.out.println(month + "个月后，兔子的总对数为：" + Fibonacci.fibonacciByMatrix(month));
     }
 }
